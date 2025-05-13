f0f21216668895aae3db778d4c259e1ec215cd41
aditirathi <aditihrathi@icloud.com>
2025-05-13 13:49:53 -0400

    button updates, assignment page updates

--- src/Kambaz/Courses/Assignments/index.tsx
+++ src/Kambaz/Courses/Assignments/index.tsx
@@ -5,6 +5,9 @@ export default function Assignments() {
              id="wd-search-assignment" />
       <button id="wd-add-assignment-group">+ Group</button>
       <button id="wd-add-assignment">+ Assignment</button>
+      <button id="wd-add-quiz">+ Quiz</button>
+      <button id="wd-add-exam">+ Exam</button> 
+      <button id="wd-add-project">+ Project</button>
       <h3 id="wd-assignments-title">
         ASSIGNMENTS 40% of Total <button>+</button> </h3>
       <ul id="wd-assignment-list">
@@ -31,5 +34,11 @@ export default function Assignments() {
           <br></br> Multiple Modules | <strong>Not available until</strong> May 20 @ 12:00 AM
             <br></br> <strong>Due</strong> May 27 @ 11:59 PM | 100 Points</li>
       </ul>
+      <h3 id="wd-quiz-title">
+        QUIZZES 10% of Total <button>+</button> </h3>
+      <h3 id="wd-exam-title">
+        EXAMS 20% of Total <button>+</button> </h3>
+      <h3 id="wd-quiz-title">
+        PROJECT 30% of Total <button>+</button> </h3>
     </div>
 );}
--- src/Kambaz/Courses/Home/Status.tsx
+++ src/Kambaz/Courses/Home/Status.tsx
@@ -8,8 +8,8 @@ export default function CourseStatus() {
       <div> <button> Import Existing Content</button> </div>
       <div> <button> Import From Commons</button> </div>
       <div> <button> Choose Home Page</button> </div>
-      <div> <button> Import Existing Content</button> </div>
-      <div> <button> Import Existing Content</button> </div>
-      <div> <button> Import Existing Content</button> </div>
-      <div> <button> Import Existing Content</button> </div>
+      <div> <button> View Course Stream</button> </div>
+      <div> <button> New Announcement</button> </div>
+      <div> <button> New Analytics</button> </div>
+      <div> <button> View Course Notifications</button> </div>
     </div> );}

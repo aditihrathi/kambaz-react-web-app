3f0bd72470978d56a11ece35bd46972d31e8da71
aditirathi <aditihrathi@icloud.com>
2025-05-26 21:59:59 -0700

    need to do 3.8.8

--- src/Kambaz/Courses/Navigation.tsx
+++ src/Kambaz/Courses/Navigation.tsx
@@ -1,34 +1,29 @@
-import { NavLink, useParams } from "react-router-dom";
+import { NavLink, useParams, useLocation } from "react-router-dom";
 
 export default function CourseNavigation() {
   const { cid } = useParams();
+  const { pathname } = useLocation();
 
-  const links = [
-    { name: "Home", path: "Home" },
-    { name: "Modules", path: "Modules" },
-    { name: "Piazza", path: "Piazza" },
-    { name: "Zoom Meetings", path: "Zoom" },
-    { name: "Assignments", path: "Assignments" },
-    { name: "Quizzes", path: "Quizzes" },
-    { name: "Grades", path: "Grades" },
-    { name: "People", path: "People" }
-  ];
+  const links = ["Home", "Modules", "Piazza", "Zoom", "Assignments", "Quizzes", "Grades", "People"];
 
   return (
     <div id="wd-courses-navigation" className="wd list-group fs-5 rounded-0">
-      {links.map((link) => (
-        <NavLink
-          key={link.name}
-          to={`/Kambaz/Courses/${cid}/${link.path}`}
-          className={({ isActive }) =>
-            `list-group-item border-0 ps-3 ${
+      {links.map((link) => {
+        const path = `/Kambaz/Courses/${cid}/${link}`;
+        const isActive = pathname === path;
+
+        return (
+          <NavLink
+            key={link}
+            to={path}
+            className={`list-group-item border-0 ps-3 ${
               isActive ? "text-dark fw-bold border-start border-black" : "text-danger"
-            }`
-          }
-        >
-          {link.name}
-        </NavLink>
-      ))}
+            }`}
+          >
+            {link}
+          </NavLink>
+        );
+      })}
     </div>
   );
-        }
+}
